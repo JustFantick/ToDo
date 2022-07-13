32338f0db742cb2c2c8dda759df8f206fb49ec13
SirKotelok <kotelevskijdanil0@gmail.com>
2022-07-13 19:47:08 +0300

    Added posibility to swipe sidebar on mobile

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -8,8 +8,36 @@ class Sidebar extends Component {
 		this.openPopup = this.openPopup.bind(this);
 	}
 
-	openPopup() {
-		document.querySelector('.popup').classList.add('active');
+	openPopup() { document.querySelector('.popup').classList.add('active'); }
+
+	componentDidMount() {
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i
+			.test(navigator.userAgent)) {
+			let x1, y1;
+
+			document.querySelector('.sidebar').addEventListener('touchstart', handleTouchStart);
+			document.querySelector('.sidebar').addEventListener('touchmove', handleTouchMove);
+
+			function handleTouchStart(e) {
+				const firstTouch = e.touches[0];
+				x1 = firstTouch.clientX;
+				y1 = firstTouch.clientY;
+			}
+
+			function handleTouchMove(e) {
+				let x2 = e.touches[0].clientX,
+					y2 = e.touches[0].clientY,
+					xDiff = x2 - x1,
+					yDiff = y2 - y1;
+
+				if (Math.abs(xDiff) > Math.abs(yDiff) && xDiff > 0) {
+					document.querySelector('.wrapper').classList.remove('active');
+					document.querySelectorAll('.task').forEach((task) => {
+						task.classList.remove('active');
+					});
+				}
+			}
+		}
 	}
 
 	render() {
