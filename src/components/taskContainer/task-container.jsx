45ad517e2e913175b0e7180a4d583b2612e31cdc
JustFantick <68466135+JustFantick@users.noreply.github.com>
2022-07-28 21:21:15 +0300

    Changed visual effect while scrolling large number of tasks

--- src/components/taskContainer/task-container.jsx
+++ src/components/taskContainer/task-container.jsx
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import Task from '../task/task.jsx';
 
 class TaskContainer extends Component {
+	constructor(props) {
+		super(props)
+		this.scrollHandler = this.scrollHandler.bind(this);
+	}
+
+	componentDidMount() {
+		let headerHeight = document.querySelector('.header').offsetHeight;
+
+		this.taskContainerBody = document.querySelector('.tasks-container__body');
+		this.taskContainerBody.style.paddingTop = headerHeight + 'px';
+	}
+
+	scrollHandler() {
+		let header = document.querySelector('.header');
+		let main = document.querySelector('.main');
+
+		if (this.taskContainerBody.scrollTop > main.clientTop) {
+			header.classList.add('scrolled');
+		} else {
+			header.classList.remove('scrolled');
+		}
+	}
+
 	render() {
 		return (
 			<div className="tasks-container">
-				<div className='tasks-container__body'>
+				<div
+					className='tasks-container__body'
+					onScroll={this.scrollHandler}
+				>
 					{
 						this.props.tasks.map((task, index) => (
 							<Task key={index} index={index} title={task.title} chooseTask={this.props.chooseTask}
